de06669c1e40d306514b5929f1033ad227ee07a6
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2021-11-11 23:54:22 +0100

    Create PrototypeDebug.cs

---
+++ Assets/Scripts/PrototypeDebug.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrototypeDebug : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("nature is alive!");
+        Debug.LogWarning("so care about the nature!");
+        Debug.LogError("don't the destroy nature!");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Debug.Log("spaaaaaaaaaaaaaaaaaaam!"); 
+    }
+}
