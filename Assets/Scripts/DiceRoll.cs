513eb779e6cfce62a75295a67574c5766586c2d2
Zeltoss <92942461+Zeltoss@users.noreply.github.com>
2021-11-18 19:32:09 +0100

    Create DiceRoll.cs

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour
+{
+
+    private string winMessage = "GEWONNEN! Deine Nummer lautet: ";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Lucky Number Zahlen: 6, 13 und 17. Drücke Space um zu würfeln. Viel Glück!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            int diceRoll = Random.Range(1, 20);
+
+            if (diceRoll == 6 || diceRoll == 13 || diceRoll == 17)
+            {
+                Debug.Log(winMessage + diceRoll);
+            }
+            else
+            {
+                Debug.Log("VERLOREN! Deine Nummer lautet: " + diceRoll);
+            }
+        }
+
+    }
+}
